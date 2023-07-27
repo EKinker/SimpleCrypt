8f0ee2f3168456842872aa5ff91c10084672a7e3
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-26 22:10:53 -0400

    Rot13

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -2,9 +2,11 @@ import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
 
     ROT13(Character cs, Character cf) {
+
+
     }
 
     ROT13() {
@@ -12,8 +14,17 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        char[] charArray = text.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M')) {
+                c += 13;
+            } else if ((c > 'm' && c <= 'z') || (c > 'M' && c <= 'Z')) {
+                c -= 13;
+            }
+            charArray[i] = c;
+        }
+        return new String(charArray);
     }
 
     public String encrypt(String text) {
--- Crypto/src/ROT13Test.java
+++ Crypto/src/ROT13Test.java
@@ -1,3 +1,4 @@
+import com.sun.jdi.CharValue;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
